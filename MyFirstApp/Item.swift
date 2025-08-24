8f6ffb8d07593a01dc023659570036cdc1b5bf54
Varun Patel <varun1329@icloud.com>
2025-08-24 12:34:22 -0400

    Initial Commit

---
+++ MyFirstApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MyFirstApp
+//
+//  Created by Varun Patel on 8/24/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
